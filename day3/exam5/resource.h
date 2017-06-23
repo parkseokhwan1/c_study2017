916243810f67b7bae20042093d475627f6205cc5
kermian <kermian@naver.com>
2017-06-23 13:40:06 +0900

    123

---
+++ day3/exam5/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// exam5.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_EXAM5_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EXAM5                       107
+#define IDI_SMALL                       108
+#define IDC_EXAM5                       109
+#define IDR_MAINFRAME                   128
+#define ID_TESTMENU_ITEM1               32771
+#define ID_TESTMENU_ITEM2               32772
+#define IDM_ITEM1                       32773
+#define IDM_ITEM2                       32774
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
