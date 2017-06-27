f7b8fd0d61d3b8c89f127456aafcf91d92aa8272
kermian <kermian@naver.com>
2017-06-27 11:07:17 +0900

    1

---
+++ day5/exam1/exam1.cpp
@@ -0,0 +1,21 @@
+// exam1.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("%d \n", 12);	//10진수
+	printf("%d \n", 014);	//8진수
+	printf("%d \n", 0xc);	//16진수
+	//printf("%d \n", b1100);
+
+	printf("%d \n", 12);
+	printf("%o \n", 12);
+	printf("%x \n", 12);
+
+
+    return 0;
+}
+
