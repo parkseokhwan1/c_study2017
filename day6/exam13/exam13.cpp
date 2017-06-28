82fc480eafdeca8eb080e5b2664c8348564b9e4f
kermian <kermian@naver.com>
2017-06-28 17:24:36 +0900

    1

---
+++ day6/exam13/exam13.cpp
@@ -0,0 +1,272 @@
+// exam13.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "exam13.h"
+
+#define MAX_LOADSTRING 100
+
+#include <Windows.h>
+#include <ObjIdl.h>
+#include <gdiplus.h>
+using namespace Gdiplus;
+#pragma comment(lib, "Gdiplus.lib")
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int g_GameLoopFSM = 0; //0 이면 랜더링준비, 1이면 랜더링
+
+int g_nPosX = 0; //전역변수
+int g_nPosY = 0;
+
+void GDIPLUS_Loop(MSG &msg)
+{
+	//gdi+ 초기화 코드
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	{
+		bool bQuit = false;
+		SolidBrush brushBlack(Color(0, 0, 0));
+		SolidBrush brushRed(Color(255, 0, 0));
+
+		Rect screenRect(0, 0, 500, 500);
+		Bitmap bmpMem(screenRect.Width, screenRect.Height);
+		Graphics *backScreen = Graphics::FromImage(&bmpMem);
+
+		double dbAngle = 0;
+
+		LONG prev_tick = 0;
+		SYSTEMTIME currentTime;
+		
+
+		while (bQuit == false) {
+			if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {
+				if (msg.message == WM_QUIT) {
+					bQuit = true;
+				}
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else {
+
+				GetSystemTime(&currentTime);
+				LONG currentTime_ms = (currentTime.wSecond*1000) + currentTime.wMilliseconds;
+
+				double dbDeltaTick = (currentTime_ms - prev_tick)/1000.0;
+
+				switch (g_GameLoopFSM)
+				{
+				case 0:
+					break;
+				case 1: {
+					HDC hdc = GetDC(msg.hwnd);
+
+					Graphics graphics(hdc);
+					backScreen->FillRectangle(&brushBlack, 0, 0, screenRect.Width, screenRect.Height);
+					backScreen->FillRectangle(&brushRed, g_nPosX, g_nPosY, 64, 64);
+
+					dbAngle += 45.0 * dbDeltaTick;
+					
+					backScreen->TranslateTransform(200, 200);
+					backScreen->RotateTransform(dbAngle);
+					backScreen->FillRectangle(&brushRed, -32, -32, 64, 64);
+					backScreen->ResetTransform();
+
+					graphics.DrawImage(&bmpMem, screenRect);
+
+					ReleaseDC(msg.hwnd, hdc);
+				}
+					break;
+				default:
+					break;
+				}
+				
+				prev_tick = currentTime_ms;
+
+			}
+		}
+	}
+
+
+	//gdi+ 종료
+	GdiplusShutdown(gdiplusToken);
+
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_EXAM13, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 응용 프로그램 초기화를 수행합니다.
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EXAM13));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다.
+	GDIPLUS_Loop(msg);
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  목적: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_EXAM13));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_EXAM13);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   설명:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  목적:  주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_RIGHT:
+			g_nPosX += 1;
+			break;
+		}
+		break;
+	case WM_ACTIVATE:
+		g_GameLoopFSM = 1;
+		break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다.
+            switch (wmId)
+            {
+			
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다.
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
