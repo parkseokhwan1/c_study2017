4c36f70e07b476c3b7ab040b79770a3730c21d49
kermian <kermian@naver.com>
2017-07-03 14:24:21 +0900

    //

--- engine/mywin32_engine.h
+++ engine/mywin32_engine.h
@@ -1,17 +1,42 @@
 #pragma once
 namespace mywin32_engine {
+	namespace util {
+
+		// , 로 구분되는 문자열을 실수형 배열을 만든다. 반환값은 생성된 실수의 갯수.
+		int Buffer2DoubleArray(TCHAR *szBuf, double *pResult) {
+			int nCount = 0;
+			TCHAR *pwc;
+			pwc = wcstok(szBuf, L", ");
+			pResult[nCount++] = _wtof(pwc);
+			while (pwc != NULL) {
+				pwc = wcstok(NULL, L", ");
+				if (pwc != NULL) {
+					pResult[nCount++] = _wtof(pwc);
+				}
+			}
+			return nCount;
+		}
+	}
 
 	void makeMiniEditBox(HWND hWnd, int nPosX, int nPosY, int nHandle)
 	{
 		CreateWindow(L"edit", NULL,
-			WS_CHILD | WS_BORDER | WS_VISIBLE,
+			WS_CHILD | WS_BORDER | WS_VISIBLE | ES_AUTOHSCROLL,
 			nPosX, nPosY, 100, 25, hWnd, (HMENU)nHandle, hInst, NULL);
+
+	}
+
+	void makeEditBox(HWND hWnd, int nPosX, int nPosY, int nWidth, int nHandle)
+	{
+		CreateWindow(L"edit", NULL,
+			WS_CHILD | WS_BORDER | WS_VISIBLE | ES_AUTOHSCROLL,
+			nPosX, nPosY, nWidth, 25, hWnd, (HMENU)nHandle, hInst, NULL);
 
 	}
 
 	void makeSimpleButton(HWND hWnd, TCHAR *pszText, int nPosX, int nPosY, int nHandle)
 	{
-		CreateWindow(L"button", L"ok", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		CreateWindow(L"button", pszText, WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
 			nPosX, nPosY, 100, 25, hWnd, (HMENU)nHandle, hInst, NULL);
 	}
 
@@ -39,7 +64,7 @@ namespace mywin32_engine {
 	void SetControlValueDouble(HWND hWnd, int nId, double nValue)
 	{
 		TCHAR szBuf[256];
-		swprintf_s(szBuf, 256, L"%lf", nValue);
+		swprintf_s(szBuf, 256, L"%.2lf", nValue);
 		SetWindowText(GetDlgItem(hWnd, nId), szBuf);
 	}
 }
