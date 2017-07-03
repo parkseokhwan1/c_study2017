428a328323e0515ff8ce4accff40e8cd317838bc
kermian <kermian@naver.com>
2017-07-03 09:26:51 +0900

    1

--- day8/exam12/exam12.cpp
+++ day8/exam12/exam12.cpp
@@ -29,7 +29,8 @@ const int g_nTileXCount = 8;
 
 void drawTile(Graphics *pGrp, Image *pImgBasicTile, int nPosX, int nPosY, int nTileIndex) {
 	//int nTileIndex = 14;
-	pGrp->DrawImage(pImgBasicTile, nPosX, nPosY,
+	pGrp->DrawImage(pImgBasicTile, 
+		Rect(nPosX, nPosY, g_nTileSize, g_nTileSize),
 		g_nTileSize * (nTileIndex % g_nTileXCount),				//원본 x 위치 
 		g_nTileSize * (nTileIndex / g_nTileXCount),				//원본 y 위치
 		g_nTileSize, g_nTileSize,
