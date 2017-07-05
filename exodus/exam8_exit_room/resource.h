bf27f09d8ce4961b855cb9eb84527e19994d18be
kermian <kermian@naver.com>
2017-07-05 17:49:44 +0900

    1

---
+++ exodus/exam8_exit_room/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// exam8_exit_room.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_EXAM8_EXIT_ROOM_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EXAM8_EXIT_ROOM             107
+#define IDI_SMALL                       108
+#define IDC_EXAM8_EXIT_ROOM             109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define IDM_START                       32772
+#define ID_32773                        32773
+#define IDM_GET_TILE                    32774
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
