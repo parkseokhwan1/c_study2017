01a7b3dbf0318d2f5c67e0d89ad9be364d2dcae5
kermian <kermian@naver.com>
2017-07-10 17:32:58 +0900

    1

---
+++ homework/member_data/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// member_data.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_MEMBER_DATA_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MEMBER_DATA                 107
+#define IDI_SMALL                       108
+#define IDC_MEMBER_DATA                 109
+#define IDR_MAINFRAME                   128
+#define DATA_DIALOG_INS                 129
+#define DATA_EDIT_INS                   1000
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define IDM_MEM_INS                     32775
+#define IDM_MEM_VIEW                    32776
+#define IDM_LOG_CLEAR                   32777
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
