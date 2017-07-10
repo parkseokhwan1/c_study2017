fd6c322c181dccdb25a5f1e82dddd24edb83c947
kermian <kermian@naver.com>
2017-07-10 15:55:30 +0900

    1

--- day14/exam6/exam6.cpp
+++ day14/exam6/exam6.cpp
@@ -2,20 +2,47 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+#include <string.h>
 
 struct Student {
+	char *m_pszName;
 	int m_nNum;
 	double m_fGrade;
 };
 
 int main()
 {
-	/*c++는 앞에 struct 생략가능*/ Student s1;
+	/* (struct) */ Student s1 = {
+		"parkseokhwan",
+		2,
+		3.14
+	};
 
+	Student s2 = {
+		"honggildong",
+		3,
+		6.28
+	};
+
+	/*s1.m_pszName = (char *)malloc(sizeof("parkseokhwan"));
+	strcpy(s1.m_pszName, "parkseokhwan");*/
+
+	/*s1.m_pszName = "parkseokhwan";
 	s1.m_nNum = 2;
-	s1.m_fGrade = 3.14;
+	s1.m_fGrade = 3.14;*/
 
-	printf("%d , %lf \n", s1.m_nNum, s1.m_fGrade);
+	printf("%s , %d , %lf \n", s1.m_pszName, s1.m_nNum, s1.m_fGrade);
+
+	Student temp;
+
+	temp = s1;		//구조체 멤버 데이터 복사
+	s1.m_fGrade = 0;
+
+	printf("s1 : %s , %d , %lf \n", s1.m_pszName, s1.m_nNum, s1.m_fGrade);
+	printf("temp : %s , %d , %lf \n", temp.m_pszName, temp.m_nNum, temp.m_fGrade);
+
+	//free(s1.m_pszName);
 
     return 0;
 }
