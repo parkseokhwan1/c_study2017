2432850213154d98cabb190ec1237a8313b64115
kermian <kermian@naver.com>
2017-07-11 14:11:12 +0900

    1

---
+++ day15/exam2/character.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct S_Character {
+	TCHAR m_szName[64];
+	int m_nHp;
+	int m_nAtk;
+	int m_nDef;
+};
---
+++ day15/exam2/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// exam2.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_EXAM2_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EXAM2                       107
+#define IDI_SMALL                       108
+#define IDC_EXAM2                       109
+#define IDR_MAINFRAME                   128
+#define IDD_NEW                         129
+#define IDC_EDIT_NAME                   1000
+#define IDC_EDIT_HP                     1001
+#define IDC_EDIT_ATK                    1002
+#define IDC_EDIT_DEP                    1003
+#define IDC_EDIT_DEF                    1003
+#define ID_GAME_32771                   32771
+#define ID_GAME_32772                   32772
+#define ID_GAME_32773                   32773
+#define IDM_NEW                         32774
+#define IDM_LOAD                        32775
+#define IDM_SAVE                        32776
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
