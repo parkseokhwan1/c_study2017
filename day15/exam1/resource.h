36205790c33e9df3e27bb8bdfccdeee69cdbefcb
kermian <kermian@naver.com>
2017-07-11 11:49:35 +0900

    1

---
+++ day15/exam1/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// exam1.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_EXAM1_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EXAM1                       107
+#define IDI_SMALL                       108
+#define IDC_EXAM1                       109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define IDM_EXAM_1                      32772
+#define ID_32773                        32773
+#define IDM_EXAM_2                      32774
+#define ID_32775                        32775
+#define IDM_EXAM_3                      32776
+#define ID_32777                        32777
+#define IDM_EXAM_4                      32778
+#define ID_32779                        32779
+#define IDM_EXAM_5                      32780
+#define ID_32781                        32781
+#define IDM_EXAM_6                      32782
+#define ID_32783                        32783
+#define IDM_EXAM_7                      32784
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32785
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
