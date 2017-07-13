24e5feaf4b72660ecbc42c7c305309e54f5d57cc
kermian <kermian@naver.com>
2017-07-13 15:18:07 +0900

    1

--- engine/plusEngine.h
+++ engine/plusEngine.h
@@ -9,4 +9,9 @@ namespace plusEngine {
 	extern void startUpGdiPlus();
 	extern void CloseGdiPlus();
 	extern void printf(Graphics *grp, int x, int y, TCHAR *fmt, ...);
+
+	//루프 제어
+	extern void(*fpOnLoop)(double);
+	extern void(*fpOnRender)(double, Graphics*);
+	extern void GDIPLUS_Loop(MSG &msg, Gdiplus::Rect rectScreen);
 }
