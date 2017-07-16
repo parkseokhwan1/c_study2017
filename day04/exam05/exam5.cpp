87f42c4600ed8acaa6f5a27b9d8480c24af23e9b
parkseokhwan1 <kermian@naver.com>
2017-07-16 21:48:46 +0900

    그림파일 불러오기

--- day04/exam05/exam5.cpp
+++ day04/exam05/exam5.cpp
@@ -158,7 +158,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다.
 
-			Image imgProfile(L"../Pepper publish.png");
+			Image imgProfile(L"../../res/Pepper publish.png");
 			Graphics graphics(hdc);
 			graphics.DrawImage(&imgProfile, 100, 100);
 			graphics.DrawImage(&imgProfile, 300, 100, 77, 8, 97, 110, UnitPixel);
